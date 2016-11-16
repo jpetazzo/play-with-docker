f7980e433b305b8cfe59ba4ecf0fc1b4b002a784
Jérôme Petazzoni <jerome.petazzoni@docker.com>
2016-11-15 19:52:06 -0800

    Update recaptcha.go

--- services/recaptcha.go
+++ services/recaptcha.go
@@ -32,28 +32,5 @@ type recaptchaResponse struct {
 }
 
 func IsHuman(req *http.Request) bool {
-	req.ParseForm()
-	challenge := req.Form.Get("g-recaptcha-response")
-
-	// Of X-Forwarded-For exists, it means we are behind a loadbalancer and we should use the real IP address of the user
-	ip := req.Header.Get("X-Forwarded-For")
-	if ip == "" {
-		// Use the standard remote IP address of the request
-
-		ip = req.RemoteAddr
-	}
-
-	parts := strings.Split(ip, ":")
-
-	resp, postErr := http.PostForm("https://www.google.com/recaptcha/api/siteverify", url.Values{"secret": {GetGoogleRecaptchaSiteSecret()}, "response": {challenge}, "remoteip": {parts[0]}})
-	if postErr != nil {
-		log.Println(postErr)
-		// If there is a problem to connect to google, assume the user is a human so we don't block real users because of technical issues
-		return true
-	}
-
-	var r recaptchaResponse
-	json.NewDecoder(resp.Body).Decode(&r)
-
-	return r.Success
+        return true
 }
